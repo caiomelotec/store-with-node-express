5a0e17a231bb4f02b883b3dccf2cdc148a6fa4d5
caiomelotec <caio.melotec@icloud.com>
2023-09-27 22:56:43 +0200

    pageNotfound / pug

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use(shopRouter); // home
 
 // error page
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+  res.status(404).render("page-not-found", { docTitle: "Page not Found" });
 });
 
 // listening
