c079d08fa7f335a4d2cec55233a8ad142e1ea801
caiomelotec <caio.melotec@icloud.com>
2023-10-01 02:47:00 +0200

    productId router

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,9 @@ const shopController = require('../controllers/shop')
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts)
-router.get("/checkout", shopController.getCheckOut)
+router.get("/products/:id", shopController.getProductId)
 router.get("/cart", shopController.getCart)
 router.get("/orders", shopController.getOrders)
+router.get("/checkout", shopController.getCheckOut)
 
 module.exports = router;
