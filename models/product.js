97a72f70ffc9d8416af17c89b86d36b43073a948
caiomelotec <caio.melotec@icloud.com>
2023-10-01 02:45:54 +0200

    findById func

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const { v4: uuidv4 } = require('uuid');
 // Construct the path to the products.json file
 const filePath = path.join(
   path.dirname(process.mainModule.filename),
@@ -8,16 +8,33 @@ const filePath = path.join(
   "products.json"
 );
 
+const getProductsFromFile = () => {
+  return new Promise((resolve, reject) => {
+    // Read the file asynchronously
+    fs.readFile(filePath, (err, fileContent) => {
+      // If there's an error reading the file, reject the Promise with the error
+      if (err) {
+        reject(err);
+      } else {
+        // If successful, parse the JSON data and resolve the Promise with the parsed data
+        resolve(JSON.parse(fileContent));
+      }
+    });
+  });
+}
+
 module.exports = class Product {
-  constructor(title, price, description, imgUrl) {
+  constructor(title, price, description, imgUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description
     this.imgUrl = imgUrl
+    this.id = id
   }
 
   save() {
     // Read the existing product data from the file
+    this.id = uuidv4()
     fs.readFile(filePath, (err, fileContent) => {
       let products = [];
       // Check if there was no error while reading the file
@@ -33,18 +50,27 @@ module.exports = class Product {
       });
     });
   }
-  static fetchAll() {
-    return new Promise((resolve, reject) => {
-      // Read the file asynchronously
-      fs.readFile(filePath, (err, fileContent) => {
-        // If there's an error reading the file, reject the Promise with the error
-        if (err) {
-          reject(err);
-        } else {
-          // If successful, parse the JSON data and resolve the Promise with the parsed data
-          resolve(JSON.parse(fileContent));
-        }
-      });
-    });
+  static async fetchAll() {
+    try {
+      return await getProductsFromFile();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const products = await getProductsFromFile();
+      const product = products.find((product) => {
+        return product.id === id
+      })
+      if(product) {
+        return product
+      } else {
+        throw new Error('Product not Found')
+      }
+    } catch(err){
+      throw err
+    }
   }
 };
