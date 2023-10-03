5795e632395079fa0264ca5180583c291efc2492
caiomelotec <caio.melotec@icloud.com>
2023-10-03 23:34:36 +0200

    /delete-product router

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const path = require("path");
 // const rootDir = require("../util/path");
-const productsController = require('../controllers/shop')
-const adminController = require('../controllers/admin')
+const productsController = require("../controllers/shop");
+const adminController = require("../controllers/admin");
 const router = express.Router();
 
 //admin/products
-router.get("/products", adminController.getProducts)
+router.get("/products", adminController.getProducts);
 
 // admin/add-product => GET
 router.get("/add-product", adminController.getAddProduct);
 // admin/add-product => POST
 router.post("/add-product", adminController.addAnewProducts);
 
-router.get('/edit-product/:id', adminController.getEditProduct)
+router.get("/edit-product/:id", adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct)
+router.post("/edit-product", adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct)
+router.post("/delete-product", adminController.postDeleteProduct);
+
 module.exports = router;
-
