93b4ffa9a181debefc70410f170c7d152ad19142
caiomelotec <caio.melotec@icloud.com>
2023-10-04 23:53:09 +0200

    getCart method

--- models/cart.js
+++ models/cart.js
@@ -66,6 +66,18 @@ class Cart {
       });
     });
   }
+
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if(err){
+        cb(null)
+      } else {
+        cb(cart)
+      }
+    })
+  }
 }
 
 module.exports = Cart;
