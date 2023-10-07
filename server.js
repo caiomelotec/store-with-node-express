8d1cae6ed90d6e6690eb3373a92a4645a16a42f0
caiomelotec <caio.melotec@icloud.com>
2023-10-08 01:23:57 +0200

    connecting to sql database

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const shopRouter = require("./routes/shop");
 const bodyParser = require("body-parser");
 const app = express();
 
+//DB
+const db = require("./util/database");
 // EJS
 app.set("view engine", "ejs");
 app.set("views", "views");
-
 
 const port = 3000;
 
@@ -19,7 +20,8 @@ app.use(express.static(path.join(__dirname, "public")));
 //routers
 app.use("/admin", adminRoutes);
 app.use(shopRouter); // home
-
+//
+db.execute("SELECT * FROM products").then().catch();
 // error page
 app.use((req, res, next) => {
   res.status(404).render("page-not-found", { docTitle: "Page not Found" });
