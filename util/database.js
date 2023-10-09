8f514578db95a7a57dedf7cc081d182fa0762fb2
caiomelotec <caio.melotec@icloud.com>
2023-10-09 03:19:16 +0200

    connecting to db with sequelize

--- util/database.js
+++ util/database.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
+const Sequelize = require("sequelize");
 
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const sequelize = new Sequelize("store", "root", process.env.PASSWORD, {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "store",
-  password: process.env.PASSWORD,
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+// const mysql = require("mysql2");
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "store",
+//   password: process.env.PASSWORD,
+// });
+
+// module.exports = pool.promise();
