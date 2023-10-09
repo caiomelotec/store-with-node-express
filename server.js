033fbf25c153f4c07dc1518c1e0373647a7ad9f1
caiomelotec <caio.melotec@icloud.com>
2023-10-09 21:40:22 +0200

    order and order-item models + relations

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 // Middleware to fetch a user
 app.use((req, res, next) => {
   User.findByPk(1)
@@ -27,7 +29,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
-// User.hasMany(CartItem);
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 // DB
 const sequelize = require("./util/database");
 
