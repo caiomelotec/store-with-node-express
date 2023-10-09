262d637c372eadffd71ea0f4c24de01ff3a9d682
caiomelotec <caio.melotec@icloud.com>
2023-10-10 00:18:03 +0200

    storing cartitems and reseting the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart.js");
+// const Cart = require("../models/cart");
+// const Order = require("../models/order");
 const formatCurrency = require("../util/formatCurrency");
 
 exports.getProducts = (req, res) => {
@@ -117,6 +118,33 @@ exports.getCheckOut = (req, res) => {
     path: "/checkout",
     docTitle: "Checkout",
   });
+};
+
+exports.postOrder = (req, res) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user.createOrder().then((order) => {
+        return order.addProducts(
+          products.map((product) => {
+            product.order_item = { quantity: product.cart_item.quantity };
+            return product;
+          })
+        );
+      });
+    })
+    .then(() => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res) => {
