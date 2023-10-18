a442c0ed52c9f5a1cea3e2fd02aad22a252c2e8d
caiomelotec <caio.melotec@icloud.com>
2023-10-18 22:41:43 +0200

    delete sequelize

--- server.js
+++ server.js
@@ -5,35 +5,19 @@ const app = express();
 require("dotenv").config();
 // console.log(process.env);
 
-// Models
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
 // Middleware to fetch a user
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
 // Relationships
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+
 // DB
-const sequelize = require("./util/database");
 
 // EJS
 app.set("view engine", "ejs");
@@ -54,27 +38,3 @@ const shopRouter = require("./routes/shop");
 // Routers
 app.use("/admin", adminRoutes);
 app.use(shopRouter); // home
-
-// DB synchronization and server listening
-sequelize
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        name: "Caio",
-        email: "caio@example.com",
-      });
-    }
-    return user;
-  })
-  .then((user) => {
-    // console.log(user);
-    user.createCart();
-  })
-  .then(() => {
-    app.listen(port);
-  })
-  .catch((err) => console.log(err));
