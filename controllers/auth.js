c978eab1b54b64dec42adeb987f3e6977bdd7a3b
caiomelotec <caio.melotec@icloud.com>
2023-11-27 10:49:41 +0100

    post login and logout with session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,33 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res) => {
-  const cookieString = req.get("Cookie");
-  const isAuth = cookieString.split("=")[1];
-  console.log(isAuth);
+  // const cookieString = req.get("Cookie");
+  // const isAuth = cookieString?.split("=")[1];
+  // console.log(isAuth);
+  console.log(req.session.isAuth);
   res.render("auth/login", {
     path: "/login",
     docTitle: "Login",
-    isAuth: isAuth,
+    isAuth: req.session.isAuth,
   });
 };
 
 exports.postLogin = (req, res) => {
-  req.isAuth = true;
-  res.setHeader("Set-Cookie", "isAuth=true");
-  res.redirect("/");
+  User.findById("6562e11c104e1410408d23b8")
+    .then((user) => {
+      req.session.isAuth = user;
+      req.session.user = user;
+      // console.log(req.session.user);
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
