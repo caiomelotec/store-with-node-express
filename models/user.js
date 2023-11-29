0fe61854faba6a81461f87b625c2378e7e4d29e6
caiomelotec <caio.melotec@icloud.com>
2023-11-29 21:30:51 +0100

    insert password in userSchema

--- models/user.js
+++ models/user.js
@@ -5,9 +5,12 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: {
     type: String,
-    require: true,
   },
   email: {
+    type: String,
+    require: true,
+  },
+  password: {
     type: String,
     require: true,
   },
