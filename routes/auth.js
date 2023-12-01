cdc4f40eccee5d51294abba2a7b3f5088ea88674
caiomelotec <caio.melotec@icloud.com>
2023-12-01 05:22:06 +0100

    reset password routers

--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,9 @@ router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get("/reset", authController.getResetPassword);
+router.post("/reset", authController.postResetPassword);
+router.get("/reset/:token", authController.getSetNewPassword);
+router.post("/reset/:token", authController.patchResetPassword);
 
 module.exports = router;
