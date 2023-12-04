10380ac4fc0f5e46503aad17f2e7b29f06156286
caiomelotec <caio.melotec@icloud.com>
2023-12-04 23:04:12 +0100

    getInvoice Streaming data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require("../models/product");
 // const Cart = require("../models/cart");
+const fs = require("fs");
+const path = require("path");
 const Order = require("../models/order");
 const formatCurrency = require("../util/formatCurrency");
 
@@ -146,5 +148,31 @@ exports.getOrders = (req, res) => {
     })
     .catch((err) => {
       console.log("Error fetching orders:", err);
+    });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No Order found"));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized"));
+      }
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"'
+      );
+      file.pipe(res);
+    })
+    .catch((err) => {
+      next(err);
     });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -12,5 +12,6 @@ router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 router.post("/create-order", isAuth, shopController.postOrder);
 router.get("/orders", isAuth, shopController.getOrders);
 router.get("/checkout", isAuth, shopController.getCheckOut);
+router.get("/orders/:orderId", isAuth, shopController.getInvoice);
 
 module.exports = router;
