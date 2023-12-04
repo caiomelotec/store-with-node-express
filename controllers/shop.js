c9c301da2a1c9c7fdbfbe66c21330d758583cf1e
caiomelotec <caio.melotec@icloud.com>
2023-12-04 23:49:47 +0100

    Generating pdf files

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,9 +1,11 @@
 const Product = require("../models/product");
+const PDFDocument = require("pdfkit");
 // const Cart = require("../models/cart");
 const fs = require("fs");
 const path = require("path");
 const Order = require("../models/order");
 const formatCurrency = require("../util/formatCurrency");
+const product = require("../models/product");
 
 exports.getProducts = (req, res) => {
   Product.find()
@@ -163,14 +165,39 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
-
-      const file = fs.createReadStream(invoicePath);
+      // generate a pdf
+      const pdfDoc = new PDFDocument();
       res.setHeader("Content-Type", "application/pdf");
       res.setHeader(
         "Content-Disposition",
         'inline; filename="' + invoiceName + '"'
       );
-      file.pipe(res);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
+      });
+      pdfDoc.text("-----------------------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              prod.product.price +
+              "€ "
+          );
+      });
+      pdfDoc.fontSize(26).text("-----------------------------------");
+      pdfDoc.fontSize(14).text("Total Price: " + totalPrice + "€");
+      // const file = fs.createReadStream(invoicePath);
+      pdfDoc.end();
+
+      // file.pipe(res);
     })
     .catch((err) => {
       next(err);
